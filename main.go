cd6810cc5c1fbf7d0f3af24b5c7c8d1392157b9c
Kunal-deve1oper <ghoshkunal106@gmail.com>
2024-02-28 23:07:59 +0530

    updated main.go file

--- main.go
+++ main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
 	"github.com/Kunal-deve1oper/md-to-html/pkg/fileparse"
 	"github.com/Kunal-deve1oper/md-to-html/pkg/mdparser"
 )
 
-func main() {
-	filepath := flag.String("f", "", "path to the markdown file")
-	flag.Parse()
-	if *filepath == "" {
-		fmt.Println("Error: Please provide the path to the markdown file using -f flag.")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+// func main() {
+// 	filepath := flag.String("f", "", "path to the markdown file")
+// 	flag.Parse()
+// 	if *filepath == "" {
+// 		fmt.Println("Error: Please provide the path to the markdown file using -f flag.")
+// 		flag.PrintDefaults()
+// 		os.Exit(1)
+// 	}
+// 	v := &fileparse.FileContent{}
+// 	err := v.ParseMd(*filepath)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+// 	t := &mdparser.MdToHtml{}
+// 	err = t.HtmlParser(v.Content)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+// 	fmt.Println("HTML conversion successful!")
+// }
+
+func Convert(filename string) error {
 	v := &fileparse.FileContent{}
-	err := v.ParseMd(*filepath)
+	err := v.ParseMd(filename)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 	t := &mdparser.MdToHtml{}
 	err = t.HtmlParser(v.Content)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
-	fmt.Println("HTML conversion successful!")
+	return nil
 }
